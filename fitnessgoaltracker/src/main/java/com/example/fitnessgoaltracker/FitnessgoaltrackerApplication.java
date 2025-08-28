4b8c3e1f9f1431002fa5e5ee79bab09027521f5b
Nithish kumar B <bnithishkumar2023@gmail.com>
2025-08-28 23:04:34 +0530

    Initial commit

---
+++ fitnessgoaltracker/src/main/java/com/example/fitnessgoaltracker/FitnessgoaltrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitnessgoaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessgoaltrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessgoaltrackerApplication.class, args);
+	}
+
+}
---
+++ fitnessgoaltracker/src/test/java/com/example/fitnessgoaltracker/FitnessgoaltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnessgoaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessgoaltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
